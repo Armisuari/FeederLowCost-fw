1026565c9b2bd10f6a99b3b0adb27c58a9df661f
Arief Nur Misuari <arief.nur@efishery.com>
2024-06-13 18:31:19 +0700

    refactor(main) : resolve not working millis scheduling

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,24 @@
 #include <Arduino.h>
 #include <FeederApplication.h>
 
-struct TaskUtils
-{
-  uint16_t interval;   // Time interval in milliseconds
-  uint32_t prevMillis; // Last execution time
-};
-
-void setTaskScheduler(bool (*func)(), uint16_t interval);
-
 FeederApplication app;
 
 void setup()
 {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   app.init();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  setTaskScheduler([]() { return app.measureLoad(); }, 1000U);
-}
+  // setTaskScheduler([]() { return app.measureLoad(); }, 10000U);
 
-void setTaskScheduler(bool (*func)(), uint16_t interval)
-{
-  TaskUtils _task;
-  _task.interval = interval;
-  uint32_t currentMillis = millis();
-  if (currentMillis - _task.prevMillis >= _task.interval)
+  static uint32_t lastUpadate = 0;
+  if (millis() - lastUpadate >= 1000U)
   {
-    _task.prevMillis = currentMillis;
-    func(); // Call the function
+    lastUpadate = millis();
+    app.measureLoad();
   }
 }
