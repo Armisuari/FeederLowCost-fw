4332abba2f13999a225a3ee110f70567680713a9
Arief Nur Misuari <arief.nur@efishery.com>
2024-06-19 14:17:08 +0700

    feat : implement test cases on main to simulate feeding process

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,20 @@
 #include "AppConstant.h"
 
 #include <FeederApplication.h>
+#include <driver/LoadCell_HX71708.h>
 #include <driver/Gate_servo.h>
 #include <driver/Thrower_relay.h>
 
+LoadCell_HX71708 hx71708;
 Gate_servo servo;
 Thrower_relay relay;
 
-FeederApplication app(servo, relay);
+FeederApplication app(hx71708, servo, relay);
 
 void serialCommand();
+
+bool isFeeding = false;
+FeedingMode fm;
 
 void setup()
 {
@@ -25,17 +30,21 @@ void loop()
   // put your main code here, to run repeatedly:
   serialCommand();
 
+  if (isFeeding)
+  {
+    app.feed(isFeeding, fm);
+  }
+
   static uint32_t lastUpadate = 0;
-  if (millis() - lastUpadate >= 100U)
+  if (millis() - lastUpadate >= 1000U)
   {
     lastUpadate = millis();
-    // app.measureLoad();
+    app.measureLoad();
   }
 }
 
 void serialCommand()
 {
-  int _percent = 0;
   // Check if data is available to read
   if (Serial.available() > 0)
   {
@@ -45,5 +54,31 @@ void serialCommand()
 
     // Print the received command
     log_i("Received command: %s", input.c_str());
+
+    if (input == "feed1")
+    {
+      // feed
+      isFeeding = true;
+      fm = MINIMUM;
+    }
+    else if (input == "feed2")
+    {
+      isFeeding = true;
+      fm = MEDIUM;
+    }
+    else if (input == "feed3")
+    {
+      isFeeding = true;
+      fm = MAXIMUM;
+    }
+    // else if (input == "feed0")
+    // {
+    //   // stop
+    //   isFeeding = false;
+    // }
+    else
+    {
+      log_e("invalid command");
+    }
   }
 }
