e0ba57df19fb5ccec426c91fca032b1d835cab62
Arief Nur Misuari <arief.nur@efishery.com>
2024-06-26 14:13:45 +0700

    fix(Gate_servo) : fixing servo init argumen and re-mapping value of degree convertion

--- src/driver/Gate_servo.cpp
+++ src/driver/Gate_servo.cpp
@@ -7,18 +7,20 @@ Gate_servo::Gate_servo(int pin) noexcept : _pin(pin)
 bool Gate_servo::init() noexcept
 {
     servo.setPeriodHertz(50);           // Standard 50hz servo
-    servo.attach(_pin, 1000, 2000);     // Attaches the servo on the pin to the servo object
+    servo.attach(_pin, 500, 2400);     // Attaches the servo on the pin to the servo object
                                         // using MG995 large servo min/max of 1000us and 2000us.
     return true;
 }
 
 bool Gate_servo::open(int percent) noexcept
 {
-    servo.write(percentToDegree(percent));
+    int degree = percentToDegree(percent);
+    log_d("servo move to: %d", degree);
+    servo.write(degree);
     return true;
 }
 
 int Gate_servo::percentToDegree(int percent) noexcept
 {
-    return map(percent, 0, 100, 0, 35);
+    return map(percent, 0, 100, 35, 0);
 }
